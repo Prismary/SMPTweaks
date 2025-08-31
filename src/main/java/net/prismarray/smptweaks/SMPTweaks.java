de0f22947b4af14c25a87511288523084f48413e
Clemens Nowitzki <clemens@nowitzki.org>
2025-08-31 10:50:18 +0200

    Create plugin files

---
+++ src/main/java/net/prismarray/smptweaks/SMPTweaks.java
@@ -0,0 +1,17 @@
+package net.prismarray.smptweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPTweaks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
